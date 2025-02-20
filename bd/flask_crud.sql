4370966f7af3452f2a2c35d660e6c33bd48e9974
Dr. Ahmed SEJAD <53151316+ahmedsejad1@users.noreply.github.com>
2025-02-20 12:52:48 +0000

    20-02-2025

--- bd/flask_crud.sql
+++ bd/flask_crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 18 fév. 2025 à 00:19
+-- Généré le : jeu. 20 fév. 2025 à 12:51
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.13
 
@@ -32,14 +32,15 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `categories`
 --
 
 INSERT INTO `categories` (`id`, `nom`) VALUES
-(1, 'Voitures');
+(1, 'Voitures'),
+(2, 'Maisons');
 
 -- --------------------------------------------------------
 
@@ -56,15 +57,15 @@ CREATE TABLE IF NOT EXISTS `items` (
   `category_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `items`
 --
 
 INSERT INTO `items` (`id`, `name`, `description`, `image_path`, `category_id`) VALUES
-(35, 'aa', 'aaa', 'static/uploads/WhatsApp_Image_2025-01-16_at_12.06.59.jpeg', 1),
-(36, 'bb', 'bbbb', 'static/uploads/corolla.jpg', 1);
+(37, 'Maison', 'Maison de luxe', 'static/uploads/MAISON.jpg', 2),
+(36, 'Corolla', 'Corolla 2020', 'static/uploads/corolla.jpg', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
